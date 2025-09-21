31ebbb9d61f2139f3f67cd77012229c0aa69822b
marmar-biz <biazar.insurance@gmail.com>
2025-09-21 23:02:11 +0330

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Nav />
-      <Component {...pageProps} />
+      <main>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
