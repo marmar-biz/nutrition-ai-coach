d17114b9b0cf98eba179ae64fcc1fb5139876e2c
marmar-biz <biazar.insurance@gmail.com>
2025-09-21 21:26:07 +0330

    add Nav component

---
+++ components/Nav.js
@@ -0,0 +1,8 @@
+export default function Nav() {
+  return (
+    <nav style={{ padding: "10px", background: "#f0f0f0" }}>
+      <a href="/" style={{ marginRight: "10px" }}>Home</a>
+      <a href="/about">About</a>
+    </nav>
+  );
+}
